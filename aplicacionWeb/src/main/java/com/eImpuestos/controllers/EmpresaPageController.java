c7ef627f35eba7d389ff1d383c5057c151c6924c
Nico-pcCasa <nicolas.ard@gmail.com>
2017-06-20 19:19:46 -0300

    Se agrego un banner con la empresa. y se corrigio el acceso a la pagina de inicio sin tener variables de session. Poner esto a futuro usando decoradores

--- aplicacionWeb/src/main/java/com/eImpuestos/controllers/EmpresaPageController.java
+++ aplicacionWeb/src/main/java/com/eImpuestos/controllers/EmpresaPageController.java
@@ -4,17 +4,22 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.List;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import java.util.Map;
 import java.util.HashMap;
 
 @Controller
 public class EmpresaPageController {
     @RequestMapping("/")
-    public String inicio(){ 
-    	return "inicio";
+    public String inicio(HttpServletRequest request){ 
+        if(request.getSession().getAttribute("empresaSeleccionada") == null){
+            return "redirect:/seleccioneEmpresa";   
+        }
+        return "inicio";
     }
     
     @RequestMapping("/seleccioneEmpresa")
