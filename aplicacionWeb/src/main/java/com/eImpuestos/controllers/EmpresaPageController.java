2300a80509b9dc80771d434f380cface48e8aadc
Nico-pcCasa <nicolas.ard@gmail.com>
2017-06-20 10:52:52 -0300

    Agregado el manejo de templates

---
+++ aplicacionWeb/src/main/java/com/eImpuestos/controllers/EmpresaPageController.java
@@ -0,0 +1,25 @@
+package com.eImpuestos.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
+import java.util.HashMap;
+
+@Controller
+public class EmpresaPageController {
+    
+    @RequestMapping("/seleccioneEmpresa")
+    public String home(){ 
+    	return "seleccioneEmpresa";
+    }
+    
+    @RequestMapping("/facturacion")
+    public String facturacion(){ 
+    	return "facturacion";
+    }
+}
