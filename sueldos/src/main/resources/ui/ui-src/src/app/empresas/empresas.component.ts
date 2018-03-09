10eb7599da42713a5a2df9ea9bbcf29e6e5a14a7
Nic Ard <nicolas.ard@gmail.com>
2018-03-08 22:54:44 -0300

    Se agrega proyecto ui-src de angular

---
+++ sueldos/src/main/resources/ui/ui-src/src/app/empresas/empresas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Empresa } from '../empresa';
+
+@Component({
+  selector: 'app-empresas',
+  templateUrl: './empresas.component.html',
+  styleUrls: ['./empresas.component.css']
+})
+export class EmpresasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
